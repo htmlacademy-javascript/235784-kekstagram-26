6705769275026d15badb494c7db1f085b5cbf307
work-account-p <97277333+work-account-p@users.noreply.github.com>
2022-07-04 17:42:34 +0300

    Вывод превью изображений на экран

--- js/rendering.js
+++ js/rendering.js
@@ -6,4 +6,18 @@
 Количество комментариев comments выведите в блок .picture__comments.
 Отрисуйте сгенерированные DOM-элементы в блок .pictures. Для вставки элементов используйте DocumentFragment.
  */
+const pictureBlock = document.querySelector('.pictures');
+const rendering = function (renderObjects) {
+  const renderingItem = `
+   <a href="#" class="picture">
+      <img class="picture__img" src="${renderObjects.url}" width="182" height="182" alt="Случайная фотография">
+      <p class="picture__info">
+        <span class="picture__comments">${renderObjects.comments.length}</span>
+        <span class="picture__likes">${renderObjects.likes}</span>
+      </p>
+    </a>
+  `;
+  pictureBlock.innerHTML += renderingItem;
+};
 
+export {rendering};
