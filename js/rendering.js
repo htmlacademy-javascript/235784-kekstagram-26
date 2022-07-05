3909e2f3cd7586cf911367195e6f960e1669e4a4
work-account-p <97277333+work-account-p@users.noreply.github.com>
2022-07-05 09:13:12 +0300

    Убрал console.log

--- js/rendering.js
+++ js/rendering.js
@@ -1,27 +1,16 @@
-/*
-На основе временных данных для разработки и шаблона #picture создайте DOM-элементы, соответствующие фотографиям, и заполните их данными:
-
-Адрес изображения url подставьте как атрибут src изображения.
-Количество лайков likes выведите в блок .picture__likes.
-Количество комментариев comments выведите в блок .picture__comments.
-Отрисуйте сгенерированные DOM-элементы в блок .pictures. Для вставки элементов используйте DocumentFragment.
- */
 const pictures = document.querySelector('.pictures');
 const pictureItem = document.querySelector('#picture').content.querySelector('.picture');
 const template = document.createDocumentFragment();
 
 const rendering = function (renderObjects) {
-  console.log(renderObjects);
-  console.log(renderObjects.url);
   const imageItem = pictureItem.cloneNode(true);
-  console.log(imageItem);
   imageItem.querySelector('.picture__img').src = renderObjects.url;
   imageItem.querySelector('.picture__likes').textContent = renderObjects.likes;
   imageItem.querySelector('.picture__comments').textContent = renderObjects.comments.length;
   pictures.appendChild(template.appendChild(imageItem));
 };
 
-  /*
+/*
   const pictureBlock = document.querySelector('.pictures');
   const rendering = function (renderObjects) {
     const renderingItem = `
@@ -35,7 +24,7 @@ const rendering = function (renderObjects) {
     `;
     pictureBlock.innerHTML += renderingItem;
   };
-  */
+*/
 
 
 export {rendering};
