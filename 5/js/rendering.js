0aa8f57687b80cefcc31d4d325920cd01ee57120
Keks <keks@htmlacademy.ru>
2022-07-05 09:13:24 +0300

    :heavy_check_mark: Сборка #5

--- 5/js/rendering.js
+++ 5/js/rendering.js
@@ -1,23 +1,30 @@
-/*
-На основе временных данных для разработки и шаблона #picture создайте DOM-элементы, соответствующие фотографиям, и заполните их данными:
+const pictures = document.querySelector('.pictures');
+const pictureItem = document.querySelector('#picture').content.querySelector('.picture');
+const template = document.createDocumentFragment();
 
-Адрес изображения url подставьте как атрибут src изображения.
-Количество лайков likes выведите в блок .picture__likes.
-Количество комментариев comments выведите в блок .picture__comments.
-Отрисуйте сгенерированные DOM-элементы в блок .pictures. Для вставки элементов используйте DocumentFragment.
- */
-const pictureBlock = document.querySelector('.pictures');
 const rendering = function (renderObjects) {
-  const renderingItem = `
-   <a href="#" class="picture">
-      <img class="picture__img" src="${renderObjects.url}" width="182" height="182" alt="Случайная фотография">
-      <p class="picture__info">
-        <span class="picture__comments">${renderObjects.comments.length}</span>
-        <span class="picture__likes">${renderObjects.likes}</span>
-      </p>
-    </a>
-  `;
-  pictureBlock.innerHTML += renderingItem;
+  const imageItem = pictureItem.cloneNode(true);
+  imageItem.querySelector('.picture__img').src = renderObjects.url;
+  imageItem.querySelector('.picture__likes').textContent = renderObjects.likes;
+  imageItem.querySelector('.picture__comments').textContent = renderObjects.comments.length;
+  pictures.appendChild(template.appendChild(imageItem));
 };
+
+/*
+  const pictureBlock = document.querySelector('.pictures');
+  const rendering = function (renderObjects) {
+    const renderingItem = `
+     <a href="#" class="picture">
+        <img class="picture__img" src="${renderObjects.url}" width="182" height="182" alt="Случайная фотография">
+        <p class="picture__info">
+          <span class="picture__comments">${renderObjects.comments.length}</span>
+          <span class="picture__likes">${renderObjects.likes}</span>
+        </p>
+      </a>
+    `;
+    pictureBlock.innerHTML += renderingItem;
+  };
+*/
+
 
 export {rendering};
