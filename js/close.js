0240c4d9003afa7f9f3bf551cc0252633eb9ac15
work-account-p <97277333+work-account-p@users.noreply.github.com>
2022-07-12 17:53:24 +0300

    Правда или действие 1. Появление и закрытие окна редактирования.

--- js/close.js
+++ js/close.js
@@ -1,17 +1,32 @@
 /*Закрытие popup по клику на крестик или кнопке Escape */
 const closeModal = (modal) => {
   document.body.classList.remove('modal-open');
-  document.querySelector('.big-picture').classList.add('hidden');
+  document.querySelector(modal).classList.add('hidden');
 };
 
+const scaleControl = document.querySelector('.scale__control--value');
+const previewImage = document.querySelector('.img-upload__preview-image');
+
+const resetOptions = () => {
+    scaleControl.value = '100%';
+    previewImage.style.transform = 'scale(1)';
+}
+
 document.querySelector('.big-picture__cancel').addEventListener('click', () => {
-  closeModal();
+  closeModal('.big-picture');
 });
 
 document.body.addEventListener('keydown',  (evt) => {
   if(evt.keyCode === 27) {
-    closeModal();
+    closeModal('.big-picture');
+    closeModal('.img-upload__overlay');
+    resetOptions();
   }
+});
+
+document.querySelector('.img-upload__cancel').addEventListener('click', () => {
+  closeModal('.img-upload__overlay');
+  resetOptions();
 });
 
 
--- js/util.js
+++ js/util.js
@@ -1,4 +1,18 @@
 const getRandom = (firstNumber, lastNumber) => Math.round(Math.random() * (lastNumber - firstNumber) + firstNumber);
 const checkWordsCount = (wordsLine, maxSymbol) => wordsLine.length <= maxSymbol;
+const uploadImage = document.querySelector('#upload-file');
+const uploadModal = document.querySelector('.img-upload__overlay');
+const validateTag = document.querySelector('.text__hashtags');
+const validateTextComment = document.querySelector('.text__description');
+
+uploadImage.addEventListener('change',  () => {
+  uploadModal.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+});
+
+validateTag.addEventListener('keydown', () => {
+
+});
+
 
 export {getRandom,checkWordsCount};
