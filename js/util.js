bb84710b3e9e0102ca806bdf8eb31e39d6997ac4
work-account-p <97277333+work-account-p@users.noreply.github.com>
2022-07-13 09:37:54 +0300

    Добавлена проверка на переполнение текстового поля комментария

--- js/util.js
+++ js/util.js
@@ -4,6 +4,8 @@ const uploadImage = document.querySelector('#upload-file');
 const uploadModal = document.querySelector('.img-upload__overlay');
 const validateTag = document.querySelector('.text__hashtags');
 const validateTextComment = document.querySelector('.text__description');
+const uploadSubmit = document.querySelector('#upload-submit');
+const maxSymbols = 140;
 
 uploadImage.addEventListener('change',  () => {
   uploadModal.classList.remove('hidden');
@@ -12,6 +14,14 @@ uploadImage.addEventListener('change',  () => {
 
 validateTag.addEventListener('keydown', () => {
 
+});
+
+validateTextComment.addEventListener('keydown', () => {
+  if (validateTextComment.value.length >= maxSymbols) {
+    return uploadSubmit.setAttribute("disabled", "disabled");
+  } else if (validateTextComment.value.length < maxSymbols && uploadSubmit.hasAttributes("disabled")) {
+    uploadSubmit.removeAttribute("disabled");
+  }
 });
 
 
