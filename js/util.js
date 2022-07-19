6d1101f21c0f09a51aaa8e3494a97236521f2b11
work-account-p <97277333+work-account-p@users.noreply.github.com>
2022-07-19 09:18:27 +0300

    Валидация инпутов на длину символов

--- js/util.js
+++ js/util.js
@@ -1,15 +1,18 @@
 const getRandom = (firstNumber, lastNumber) => Math.round(Math.random() * (lastNumber - firstNumber) + firstNumber);
-const checkWordsCount = (wordsLine, maxSymbol) => wordsLine.length <= maxSymbol;
+const checkWordsCount = (wordsLine, maxSymbol) => wordsLine.length < maxSymbol;
 const uploadImage = document.querySelector('#upload-file');
 const uploadModal = document.querySelector('.img-upload__overlay');
-const validateTag = document.querySelector('.text__hashtags');
-const validateTextComment = document.querySelector('.text__description');
 const uploadSubmit = document.querySelector('#upload-submit');
 const maxSymbols = 140;
 const maxHashTags = 5;
 const maxHashTagsLength = 20;
 const correctEnter = /^#[a-zA-Zа-яА-ЯёЁ0-9]{0,}$/;
 const uncorrectEnter = /[^-_=+;:,.`"']$/m;
+const orderForm = document.querySelector('#upload-select-image');
+const validateTextComment = orderForm.querySelector('.text__description');
+const validateTag = orderForm.querySelector('.text__hashtags');
+const uploadFormData = document.querySelector('.img-upload__form');
+
 
 let focus = false;
 const checkEsc = (selectorCheck) => {
@@ -33,18 +36,40 @@ uploadImage.addEventListener('change',  () => {
   document.body.classList.add('modal-open');
 });
 
-validateTag.addEventListener('keydown', () => {
-  let hashTagArray = validateTag.value.split(' ');
 
+/* Валидация полей */
+
+const pristine = new Pristine(orderForm, {
+  classTo: 'img-upload__field-wrapper',
+  errorClass: 'form__item--invalid',
+  successClass: 'form__item--valid',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextTag: 'div',
+  errorTextClass: 'form__error'
 });
 
-validateTextComment.addEventListener('keydown', () => {
-  if (validateTextComment.value.length >= maxSymbols) {
-    return uploadSubmit.setAttribute("disabled", "disabled");
-  } else if (validateTextComment.value.length < maxSymbols && uploadSubmit.hasAttributes("disabled")) {
-    uploadSubmit.removeAttribute("disabled");
+const isValidateCommentLength = (value) => checkWordsCount(value, maxSymbols);
+pristine.addValidator(
+  validateTextComment,
+  isValidateCommentLength,
+  'Не больше 140 символов'
+);
+
+const isValidateTagLength = (value) => checkWordsCount(value, maxHashTagsLength);
+pristine.addValidator(
+  validateTag,
+  isValidateTagLength,
+  'Не больше 20 символов'
+);
+
+uploadFormData.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  if (!pristine.validate()) {
+    validateTag.style = 'border: 3px solid #ff0000';
+  }else{
+    validateTag.style.border = 'none';
   }
 });
 
 
-export {getRandom,checkWordsCount,validateTextComment,focus};
+export {getRandom,checkWordsCount,validateTextComment, focus};
